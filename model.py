6bb854e2c2e4e8147ce1f05080f4399a0ba580d1
Vladimir Varenik <117539159+VVV33301@users.noreply.github.com>
2023-08-13 10:21:59 +0300

    Update model

--- model.py
+++ model.py
@@ -1,16 +1,14 @@
 import tensorflow as tf
 from PIL import Image
 import numpy as np
-from random import uniform
 
-model: tf.keras.Model = tf.keras.models.load_model('./fakevsreal_weights_vova_0.h5')
+model: tf.keras.Model = tf.keras.models.load_model('fakevsreal_weights_best_1.h5')
 
 
 def classify_image(file_path: str | bytes | bytearray) -> str:
     image: Image = Image.open(file_path).resize((128, 128)).convert("RGB")
     img: np.array = np.asarray(image)
     img: np.array = np.expand_dims(img, 0)
-    predictions: tf.keras.Model.predict = model.predict(img)
-    label: str = ['реальная', 'фейковая'][np.argmax(predictions[0])]
-    rand_proba: float = round(uniform(80, 95), 2)
-    return 'Эта фотография {} с вероятностью {}%'.format(label, rand_proba)
+    predictions: tf.keras.Model.predict = model.predict(img / 255)
+    return f"Эта фотография {['реальная', 'фейковая'][np.argmax(predictions[0])]} с вероятностью " \
+           f"{round(max(predictions[0]) * 100, 2)}%"
